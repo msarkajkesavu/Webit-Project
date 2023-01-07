800a803e5af54e1b659652b04fb513aa64ee21af
Arkaj <arkajkesavms@gmail.com>
2023-01-07 09:09:35 +0530

    1.3V
    Added animation for spider movement and shooting.

--- Assets/Script/spiderscript.cs
+++ Assets/Script/spiderscript.cs
@@ -7,10 +7,11 @@ public class spiderscript : MonoBehaviour
     public Transform spider;
     public Transform webshoot;
     public GameObject webprefab;
+    private Animator spanime;
     // Start is called before the first frame update
     void Start()
     {
-
+        spanime = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,15 +21,27 @@ public class spiderscript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             spider.Translate(Vector2.right);
+            spanime.SetBool("ismove", true);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow)) 
         {
             spider.Translate(Vector2.left);
+            spanime.SetBool("ismove", true);
+        }
+        else 
+        {
+            spanime.SetBool("ismove", false);
         }
         //Webshooting Input
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
             Instantiate(webprefab, webshoot,false);
+            spanime.SetBool("isshoot", true);
+            
+        }
+        else 
+        {
+            spanime.SetBool("isshoot", false);
         }
     }
 }
