bd463357ec2408aeae1a9cb234253d5210282abb
SreeduttRamJ <sreeduttramj@gmail.com>
2023-01-08 21:38:59 +0530

    enemy final

--- Assets/Script/Enemy1prefab.cs
+++ Assets/Script/Enemy1prefab.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 public class Enemy1prefab : MonoBehaviour
 {
     private Vector2 screenBounds;
+    private Animator anime;
 
 
     void Start()
     {
+        anime = GetComponent<Animator>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        //Debug.Log("OnCollisionEnter2D");
-        //Check to see if the tag on the collider is equal to Enemy
-        if (other.gameObject.tag == "web")
+        if (other.gameObject.tag == "web" && this.gameObject.tag == "Enemy")
         {
             Destroy(other.gameObject);
+            anime.SetBool("enemy1caught", true);
+            anime.SetBool("enemyground", false);
             gameObject.tag = "eatable";
         }
-        else if (other.gameObject.tag == "ground") 
+        else if (other.gameObject.tag == "Ground" && this.gameObject.tag == "Enemy")
         {
-            Destroy(this,2);
+            Destroy(this.gameObject, 2);
         }
+        else if (other.gameObject.tag == "Ground" && this.gameObject.tag == "eatable")
+        {
+            anime.SetBool("enemyground", true);
+            anime.SetBool("enemy1caught", false);
+        }
+
+
+
 
     }
 }
